693b24741382d725a3393eb246782cb60071b355
JF <juanfran.jfaz9@gmail.com>
2025-01-13 11:42:10 +0100

    Cambio Coche

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,32 @@
+import javax.sound.sampled.AudioFileFormat;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Empleado antonio = new Empleado("Antonio","García",897,"Apple");
-        System.out.print("El empleado es:" + antonio);
+        /*System.out.print("El empleado es:" + antonio);
         Scanner sc = new Scanner(System.in);
-        String nombre2 = sc.next();
+        //String nombre2 = sc.next();
+        //antonio.setNombre(nombre2);
+        //System.out.print("El empleado es:" + antonio);
+
+         */
+        //System.out.println(antonio.getApellido());
+        //Scanner sc = new Scanner(System.in);
+        //String prueba = sc.next();
+        //antonio.setApellido(prueba);
+        //System.out.println(antonio.getApellido());
+        Coche audi = new Coche("Audi","A5", 35000.500);
+        System.out.println("Los datos del coche son:\n" + audi);
+        Scanner sc = new Scanner(System.in);
+        String marca2 = sc.next();
+        audi.setMarca(marca2);
+        String modelo2 = sc.next();
+        audi.setModelo(modelo2);
+        Double precio2 = sc.nextDouble();
+        audi.setPrecio(precio2);
+        System.out.println("Los datos del coche son:\n" + audi);
+
     }
 
 }
